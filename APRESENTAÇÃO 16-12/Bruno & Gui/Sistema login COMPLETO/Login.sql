118c48e99888ee5f47affe4496f462d99265d2c7
André Germano <48995473+JoeHermann21@users.noreply.github.com>
2026-04-13 09:16:23 -0300

    Add files via upload
    Apresentação dos alunos do curso de Desenvolvimento de sistemas da Proz. Foram feitas páginas web com sistema de login.

---
+++ APRESENTAÇÃO 16-12/Bruno & Gui/Sistema login COMPLETO/Login.sql
@@ -0,0 +1,16 @@
+create database login;
+
+use login;
+
+create table usuario (
+	id serial primary key not null,
+	email varchar (200) not null,
+	senha varchar (200) not null,
+	perfil varchar (200) check 
+	(perfil = 'ADMINISTRADOR' or perfil = 'USUARIO') not null
+);
+
+insert into usuario (email, senha, perfil) 
+values ('andre@gmail.com', '123', 'USUARIO');
+
+--select email, senha from usuario where email='' or ''='' AND senha='' or ''=''
