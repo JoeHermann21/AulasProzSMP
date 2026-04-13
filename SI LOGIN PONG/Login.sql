0bd6c8e38d38aa83f99ed5a8e72479e3d2de10d0
André Germano <48995473+JoeHermann21@users.noreply.github.com>
2026-04-13 09:24:20 -0300

    Add files via upload
    Jogo Pong com sistema de login.

---
+++ SI LOGIN PONG/Login.sql
@@ -0,0 +1,15 @@
+create database login;
+
+use login;
+
+create table usuario (
+	id serial primary key not null,
+	email varchar (200) not null,
+	senha varchar (200) not null,
+	perfil varchar (200) check (perfil = 'ADMINISTRADOR' or perfil = 'USUARIO') not null
+);
+
+insert into usuario (email, senha, perfil) values ('andre@gmail.com', '123', 'USUARIO');
+insert into usuario (email, senha, perfil) values ('zeze@gmail.com', sha1('123'), 'USUARIO');
+
+--select email, senha from usuario where email='' or ''='' AND senha='' or ''=''
